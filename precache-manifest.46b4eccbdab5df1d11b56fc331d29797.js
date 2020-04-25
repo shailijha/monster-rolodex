71a0027594f4c72390f081e1d790313b4447803b
shailijha <shaili_jha@yahoo.co.in>
2020-04-25 13:59:20 -0500

    Updates

---
+++ precache-manifest.46b4eccbdab5df1d11b56fc331d29797.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ef21aad718300806ba2121f50c7408",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4bce726802bf7ba05965",
+    "url": "/monster-rolodex/static/css/main.dfda000b.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monster-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bce726802bf7ba05965",
+    "url": "/monster-rolodex/static/js/main.55b23204.chunk.js"
+  },
+  {
+    "revision": "e4eba3394aec38fdb39c",
+    "url": "/monster-rolodex/static/js/runtime-main.2047114a.js"
+  }
+]);
